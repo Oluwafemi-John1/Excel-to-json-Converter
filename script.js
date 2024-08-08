4562b5efb1da1a68acf31753ea602078cd854b52
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-08 12:40:39 +0100

    refactor: Separate Excel to JSON conversion logic into a separate script file

---
+++ script.js
@@ -0,0 +1,31 @@
+let selectedFile;
+    document
+        .getElementById("fileUpload")
+        .addEventListener("change", function (event) {
+            selectedFile = event.target.files[0];
+        });
+
+    document
+        .getElementById("uploadExcel")
+        .addEventListener("click", function () {
+            if (selectedFile) {
+                console.log("hi");
+                var fileReader = new FileReader();
+                fileReader.onload = function (event) {
+                    var data = event.target.result;
+
+                    var workbook = XLSX.read(data, {
+                        type: "binary",
+                    });
+                    workbook.SheetNames.forEach((sheet) => {
+                        let rowObject = XLSX.utils.sheet_to_row_object_array(
+                            workbook.Sheets[sheet]
+                        );
+                        let jsonObject = JSON.stringify(rowObject);
+                        document.getElementById("jsonData").innerHTML = jsonObject;
+                        console.log(jsonObject);
+                    });
+                };
+                fileReader.readAsBinaryString(selectedFile);
+            }
+        });
